726391eb90874c27218ec30ea26978732935c12e
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-06-06 21:44:27 -0700

    add 1 image

--- js/blogArticles.js
+++ js/blogArticles.js
@@ -5,7 +5,7 @@ var articleData = [
     author: 'Patrick Colgan',
     image: 'insert src path later',
     publishedOn: '2016-5-21',
-    body: '<p>First Article Body Copy Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    body: '<p>First Article Body Copy Lorem ipsum dolor sit amet,<img src="http://lorempixel.com/400/200/" class="pull-left" > consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
   },
   {
     title: 'Marketing Analysis App',
