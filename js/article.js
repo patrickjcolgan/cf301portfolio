971b4f9526fa02782cd3bb82f9eecf97c5f2567c
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-06-19 14:03:37 -0700

    travel.json created + IFFEs

--- js/article.js
+++ js/article.js
@@ -1,44 +1,48 @@
-function Article(options) {
-  this.title = options.title;
-  this.category = options.category;
-  this.author = options.author;
-  this.image = options.image;
-  this.publishedOn = options.publishedOn;
-  this.body = options.body;
-};
+(function (module) {
 
-Article.all = [];
+  function Article(options) {
+    this.title = options.title;
+    this.category = options.category;
+    this.author = options.author;
+    this.image = options.image;
+    this.publishedOn = options.publishedOn;
+    this.body = options.body;
+  };
 
-Article.prototype.toHtml = function() {
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  var blogTemplate = $('#blog-template').html();
-  var renderTemplate = Handlebars.compile(blogTemplate);
-  return renderTemplate(this);
-};
+  Article.all = [];
 
-Article.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - new Date(a.publishedOn);
-  });
+  Article.prototype.toHtml = function() {
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    var blogTemplate = $('#blog-template').html();
+    var renderTemplate = Handlebars.compile(blogTemplate);
+    return renderTemplate(this);
+  };
 
-  rawData.forEach(function(element) {
-    Article.all.push(new Article(element));
-  });
-};
-
-
-Article.fetchAll = function() {
-  if (localStorage.rawData) {
-    console.log('localStorage exists');
-    Article.loadAll(JSON.parse(localStorage.rawData));
-    articleView.initIndexPage();
-  } else {
-    $.getJSON('/data/blogArticles.json', function(rawData) {
-      Article.loadAll(rawData);
-      localStorage.setItem('rawData', JSON.stringify(rawData));
-      articleView.initIndexPage();
-      console.log('no local storage. getJSON');
+  Article.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - new Date(a.publishedOn);
     });
-  }
-};
+
+    rawData.forEach(function(element) {
+      Article.all.push(new Article(element));
+    });
+  };
+
+
+  Article.fetchAll = function() {
+    if (localStorage.rawData) {
+      Article.loadAll(JSON.parse(localStorage.rawData));
+      articleView.initIndexPage();
+      // next();
+    } else {
+      $.getJSON('/data/blogArticles.json', function(rawData) {
+        Article.loadAll(rawData);
+        localStorage.setItem('rawData', JSON.stringify(rawData));
+        articleView.initIndexPage();
+        // next();
+      });
+    }
+  };
+  module.Article = Article;
+})(window);
--- js/articleView.js
+++ js/articleView.js
@@ -1,72 +1,75 @@
-var articleView = {};
+(function (module) {
+  var articleView = {};
 
-articleView.populateFilters = function() {
-  $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      var value = $(this).attr('data-category');
-      var optionTag = '<option value="' + value + '">' + value + '</option>';
-      if ($('#category-filter option[value="' + value + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
+  articleView.populateFilters = function() {
+    $('article').each(function() {
+      if (!$(this).hasClass('template')) {
+        var value = $(this).attr('data-category');
+        var optionTag = '<option value="' + value + '">' + value + '</option>';
+        if ($('#category-filter option[value="' + value + '"]').length === 0) {
+          $('#category-filter').append(optionTag);
+        }
       }
-    }
-  });
-};
+    });
+  };
 
-articleView.manageCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    if ($(this).val()) {
-      var $selection = $(this).val();
-      $('article').hide();
-      $('article[data-category="' + $selection + '"]').fadeIn(1000);
-      $('#category-filter option:eq(0)').text('-- Show all --');
+  articleView.manageCategoryFilter = function() {
+    $('#category-filter').on('change', function() {
+      if ($(this).val()) {
+        var $selection = $(this).val();
+        $('article').hide();
+        $('article[data-category="' + $selection + '"]').fadeIn(1000);
+        $('#category-filter option:eq(0)').text('-- Show all --');
 
-    } else {
-      $('article').fadeIn(1000);
-      $('#category-filter option:eq(0)').text('-- Filter by Category --');
-    }
-    $('article.template').hide();
-  });
-};
+      } else {
+        $('article').fadeIn(1000);
+        $('#category-filter option:eq(0)').text('-- Filter by Category --');
+      }
+      $('article.template').hide();
+    });
+  };
 
-articleView.manageMainNav = function() {
-  $('.top-nav').on('click','.tab:eq(0)', function(){
-    $('#articles').fadeIn(1000);
-    $('#about').hide();
-    $('#category-filter').show();
-  });
-  $('.top-nav').on('click','.tab:eq(1)', function(){
-    $('#about').fadeIn(1000);
-    $('#articles').hide();
-    $('#category-filter').hide();
-  });
+  articleView.manageMainNav = function() {
+    $('.top-nav').on('click','.tab:eq(0)', function(){
+      $('#articles').fadeIn(1000);
+      $('#about').hide();
+      $('#category-filter').show();
+    });
+    $('.top-nav').on('click','.tab:eq(1)', function(){
+      $('#about').fadeIn(1000);
+      $('#articles').hide();
+      $('#category-filter').hide();
+    });
 
-  $('.top-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
-};
+    $('.top-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+  };
 
-articleView.setTeasers = function() {
-  $('.teaser *:nth-of-type(n+3)').hide(); // Hide elements beyond the first 2 in any artcile body.
+  articleView.setTeasers = function() {
+    $('.teaser *:nth-of-type(n+3)').hide(); // Hide elements beyond the first 2 in any artcile body.
 
-  $('#articles').on('click', 'a.read-on', function(e) {
-    e.preventDefault();
-    $(this).parent().find('*').fadeIn();
-    $(this).hide();
-  });
-};
+    $('#articles').on('click', 'a.read-on', function(e) {
+      e.preventDefault();
+      $(this).parent().find('*').fadeIn();
+      $(this).hide();
+    });
+  };
 
-articleView.hamburgerToggle = function() {
-  $('.icon-menu').on('click', function() {
-    $('nav ul').toggle();
-  });
-};
+  articleView.hamburgerToggle = function() {
+    $('.icon-menu').on('click', function() {
+      $('nav ul').toggle();
+    });
+  };
 
-articleView.initIndexPage = function() {
-  Article.all.forEach(function(a){
-    $('#articles').append(a.toHtml());
-  });
+  articleView.initIndexPage = function() {
+    Article.all.forEach(function(a){
+      $('#articles').append(a.toHtml());
+    });
 
-  articleView.populateFilters();
-  articleView.manageCategoryFilter();
-  articleView.manageMainNav();
-  articleView.setTeasers();
-  articleView.hamburgerToggle();
-};
+    articleView.populateFilters();
+    articleView.manageCategoryFilter();
+    articleView.manageMainNav();
+    articleView.setTeasers();
+    articleView.hamburgerToggle();
+  };
+  module.articleView = articleView;
+})(window);
