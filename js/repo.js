356e58df88e54279621dabeb759db25d93704de9
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-07-10 09:56:16 -0700

    update github url

--- js/repo.js
+++ js/repo.js
@@ -6,13 +6,13 @@
   repos.requestRepos = function(callback) {
     console.log('ran request repos');
     $.ajax({
-      url: 'https://api.github.com/users/patrickjcolgan/repos?per_page=10&sorted=updated',
+      url: 'https://api.github.com/user/repos?per_page=10&sorted=updated',
       type: 'GET',
       headers: {'Authorization': 'token ' + githubToken},
       success: function(data) {
         repos.all = data;
       }
-    }).done(callback)
+    }).done(callback);
   };
 
   repos.with = function(attr) {
